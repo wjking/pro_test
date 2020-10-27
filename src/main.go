3d267d84d87af6052465d9924b572b528a515ca6
wanjin <wanjin@hrgame.com.cn>
2020-10-27 16:33:15 +0800

    [pro_test]2

--- src/main.go
+++ src/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
-	"time"
 )
 
 func main() {
-	var counter int64
-	for {
-		time.Sleep(time.Millisecond * 1000)
-		counter++
-		fmt.Printf("new counter=%d\n", counter)
+
+	input := []byte("playerName:万金")
+	encodeString := base64.StdEncoding.EncodeToString(input)
+	fmt.Print(encodeString)
+
+	decodeString, err := base64.StdEncoding.DecodeString(encodeString)
+	if err != nil {
+		return
 	}
+	fmt.Print(string(decodeString))
 }
